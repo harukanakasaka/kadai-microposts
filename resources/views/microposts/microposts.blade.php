f0d59a17e19d92501bf99bf6a8b0ef23f2ca2022
haruka.nakasaka <haruka128050@gmail.com>
2020-05-16 06:13:19 +0000

    create

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -9,6 +9,13 @@
                 <div>
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
+                <div>
+                    @if (Auth::id() == $micropost->user_id)
+                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                        {!! Form::close() !!}
+                    @endif    
+                </div>
             </div>
         </li>
     @endforeach    
